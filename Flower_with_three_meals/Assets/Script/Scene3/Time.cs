d2fe6609304d3636c07c28d691366085a3b3e45b
gaeunU <dbrkdmsyge12@naver.com>
2022-06-13 09:21:01 +0900

    Make Flower prefab
    Make flower prefab

--- Flower_with_three_meals/Assets/Script/Scene3/Time.cs
+++ Flower_with_three_meals/Assets/Script/Scene3/Time.cs
@@ -6,7 +6,7 @@ public class Time : MonoBehaviour
 {
     public GameObject day;
     public GameObject morning;
-    public GameObject night;
+   
 
     int hour = 0;
     // Start is called before the first frame update
@@ -22,28 +22,21 @@ public class Time : MonoBehaviour
     void Update()
     {
         // 6시 ~ 12시
-        if(hour == 6 || hour == 7 || hour == 8 || hour == 9 || hour == 10 || hour == 11 || hour == 12 )
+        if(hour == 1 || hour == 2 || hour == 3 || hour == 4 || hour == 5 || hour == 6 || hour == 7 )
         {
             day.SetActive(true);
             morning.SetActive(false);
-            night.SetActive(false);
+          
 
         }
-        else if (hour == 13 || hour == 14 || hour == 15 || hour == 16 || hour == 17) // 12시 ~ 17시
+        else if (hour == 8 || hour == 9 || hour == 10 || hour == 11 || hour == 12 ) // 12시 ~ 17시
         {
             morning.SetActive(true);
             day.SetActive(false);
-            night.SetActive(false);
+           
 
         }
-        else if (hour == 18 || hour == 19 || hour == 20 || hour == 21 || hour == 22 || hour == 23 ||
-            hour == 0 || hour == 1 || hour == 2 || hour == 3 || hour == 4 || hour == 5) // 17시 ~ 6시
-        {
-            night.SetActive(true);
-            day.SetActive(false);
-            morning.SetActive(false);
-
-        }
+        
        
 
     }
